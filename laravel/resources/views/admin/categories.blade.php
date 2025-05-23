3160fe6f540a187a6eca6c8aa6883c95a39b11a5
Tam909 <tamnvph49996@gmail.com>
2025-05-23 17:11:13 +0700

    Layout admin

--- laravel/resources/views/admin/categories.blade.php
+++ laravel/resources/views/admin/categories.blade.php
@@ -3,5 +3,5 @@
 @section('title', 'Quản lý danh mục sản phẩm')
 
 @section('content')
-    <p>Danh sách danh mục sản phẩm  sẽ hiển thị tại đây.</p>
+    <p>Danh sách danh mục sản phẩm  sẽ hiển thị tại đây.</p> 
 @endsection
